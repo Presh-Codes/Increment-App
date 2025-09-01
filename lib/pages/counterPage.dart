9ceca62c5c96f7886beea6e087e4d6fb9399eab6
Mr.Presh <preciousokeypraise@gmail.com>
2025-09-01 10:21:10 +0100

    chore: increment button add

--- lib/pages/counterPage.dart
+++ lib/pages/counterPage.dart
@@ -8,8 +8,38 @@ class CounterPage extends StatefulWidget {
 }
 
 class _CounterPageState extends State<CounterPage> {
+  int _counter = 0;
+
+  void _incrementer() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Vous avez appuyé sur le bouton ce nombre de fois:',
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            Text(
+              "$_counter",
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+
+            ElevatedButton(
+              onPressed: _incrementer,
+              child: Text("Increment"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
